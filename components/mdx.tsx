96ba6e12bc2afcd6c44e4929f2bb31774ac77f4a
Arjun Bhujel <arjunbhujel600@gmail.com>
2024-05-09 14:29:18 +0545

    test2

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -58,91 +58,91 @@ function Callout(props) {
   );
 }
 
-function ProsCard({ title, pros }) {
-  return (
-    <div className="border border-emerald-200 dark:border-emerald-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-4 w-full">
-      <div className="mt-4">
-        {pros.map((pro) => (
-          <div key={pro} className="flex font-medium items-baseline mb-2">
-            <div className="h-4 w-4 mr-2">
-              <svg className="h-4 w-4 text-emerald-500" viewBox="0 0 24 24">
-                <g
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
-                  <path d="M22 4L12 14.01l-3-3" />
-                </g>
-              </svg>
-            </div>
-            <span>{pro}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+// function ProsCard({ title, pros }) {
+//   return (
+//     <div className="border border-emerald-200 dark:border-emerald-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-4 w-full">
+//       <div className="mt-4">
+//         {pros.map((pro) => (
+//           <div key={pro} className="flex font-medium items-baseline mb-2">
+//             <div className="h-4 w-4 mr-2">
+//               <svg className="h-4 w-4 text-emerald-500" viewBox="0 0 24 24">
+//                 <g
+//                   fill="none"
+//                   stroke="currentColor"
+//                   strokeWidth="2"
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                 >
+//                   <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+//                   <path d="M22 4L12 14.01l-3-3" />
+//                 </g>
+//               </svg>
+//             </div>
+//             <span>{pro}</span>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
-function ConsCard({ title, cons }) {
-  return (
-    <div className="border border-red-200 dark:border-red-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-6 w-full">
-      <div className="mt-4">
-        {cons.map((con) => (
-          <div key={con} className="flex font-medium items-baseline mb-2">
-            <div className="h-4 w-4 mr-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-4 w-4 text-red-500"
-              >
-                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
-              </svg>
-            </div>
-            <span>{con}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}
+// function ConsCard({ title, cons }) {
+//   return (
+//     <div className="border border-red-200 dark:border-red-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-6 w-full">
+//       <div className="mt-4">
+//         {cons.map((con) => (
+//           <div key={con} className="flex font-medium items-baseline mb-2">
+//             <div className="h-4 w-4 mr-2">
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 viewBox="0 0 20 20"
+//                 fill="currentColor"
+//                 className="h-4 w-4 text-red-500"
+//               >
+//                 <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+//               </svg>
+//             </div>
+//             <span>{con}</span>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
-function LinkCardList({ cards }) {
-  return (
-    <div
-      className=" bg-neutral-100 dark:bg-neutral-900 rounded-xl p-6
-    my-4 w-full gap-12"
-    >
-      {cards.map((card) => (
-        <LinkCard key={card.title} title={card.title} link={card.link} />
-      ))}
-    </div>
-  );
-}
+// function LinkCardList({ cards }) {
+//   return (
+//     <div
+//       className=" bg-neutral-100 dark:bg-neutral-900 rounded-xl p-6
+//     my-4 w-full gap-12"
+//     >
+//       {cards.map((card) => (
+//         <LinkCard key={card.title} title={card.title} link={card.link} />
+//       ))}
+//     </div>
+//   );
+// }
 
-function LinkCard({ title, link }) {
-  return (
-    <Link
-      href={link}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="no-underline"
-    >
-      <button className="flex flex-row justify-start items-center w-full cursor-pointer ">
-        <div
-          className="h-4 w-4
-        mr-2"
-        >
-          <ArrowUpRight size={16} className="text-blue-700" />
-        </div>
-        <span className="font-medium text-blue-600 ">{title}</span>
-      </button>
-    </Link>
-  );
-}
+// function LinkCard({ title, link }) {
+//   return (
+//     <Link
+//       href={link}
+//       target="_blank"
+//       rel="noopener noreferrer"
+//       className="no-underline"
+//     >
+//       <button className="flex flex-row justify-start items-center w-full cursor-pointer ">
+//         <div
+//           className="h-4 w-4
+//         mr-2"
+//         >
+//           <ArrowUpRight size={16} className="text-blue-700" />
+//         </div>
+//         <span className="font-medium text-blue-600 ">{title}</span>
+//       </button>
+//     </Link>
+//   );
+// }
 
 // function Code({ children, ...props }) {
 //   let codeHTML = highlight(children);
@@ -190,11 +190,11 @@ let components = {
   Image: RoundedImage,
   a: CustomLink,
   Callout,
-  ProsCard,
-  ConsCard,
+  // ProsCard,
+  // ConsCard,
   Table,
-  LinkCardList,
-  LinkCard,
+  // LinkCardList,
+  // LinkCard,
 };
 
 export function CustomMDX(props) {
