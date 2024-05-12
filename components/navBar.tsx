87e37129f865b9b53ddc664e2a85c4475af353ef
Arjun Bhujel <arjunbhujel600@gmail.com>
2024-05-12 19:10:02 +0545

    navbar update

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,26 +1,34 @@
+'use client'
+
 import "@/styles/ui/navBar.css"
 import Link from "next/link"
+import Image from "next/image"
+
+import { usePathname } from "next/navigation"
+
 
 const navItems = {
   "/": {
-    name: "home",
-  },
-  "/projects": {
-    name: "projects",
+    name: "about",
   },
   "/blog": {
     name: "blog",
   },
+  "/project": {
+    name: "project",
+  },
 }
 
 export function Navbar() {
+  const pathname = usePathname();
+
   return (
     <div className="nav">
       <nav>
         {Object.entries(navItems).map(([path, { name }]) => {
           return (
-            <Link key={path} href={path} className="link">
-              {name}
+            <Link key={path} href={path} className={pathname === path ? "link active-page" : "link idle-page" }>
+               {name}
             </Link>
           )
         })}
